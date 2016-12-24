7e2bf1aaf2642204c03f2797230a0578f9d4f4d6
David <david@isrock.com.tw>
2016-12-24 15:22:04 +0800

    v1

---
+++ WebApplication15/BO/Health.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using isRock.Framework;
+using isRock.Framework.WebAPI;
+
+namespace WebApplication15.BO
+{
+    public class Health : BusinessLogicBase
+    {
+        /// <summary>
+        /// 計算BMI
+        /// </summary>
+        /// <param name="bodyInfo"></param>
+        /// <returns></returns>
+        public ExecuteCommandDefaultResult<float> BMI(BodyInfo bodyInfo)
+        {
+            float height = (bodyInfo.height) / 100;
+            var bmi = bodyInfo.weight / (height * height);
+            return new ExecuteCommandDefaultResult<float>()
+            {
+                Data = bmi,
+                isSuccess = true
+            };
+        }
+    }
+
+    /// <summary>
+    /// 個人身高體重
+    /// </summary>
+    public class BodyInfo
+    {
+        public float height { get; set; }
+        public float weight { get; set; }
+    }
+}
---
+++ WebApplication15/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="WebApplication15.Global" Language="C#" %>
